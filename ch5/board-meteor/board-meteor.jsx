00d35f278249f4e1d088ba8bb8ebcb80b8c63f1f
Azat Mardan <hi@azat.co>
2015-08-04 09:16:57 -0700

    fix chat

--- ch5/board-meteor/board-meteor.jsx
+++ ch5/board-meteor/board-meteor.jsx
@@ -1,32 +1,23 @@
-/**
- * This directive is necessary to enable preprocessing of JSX tags:
- * @jsx React.DOM
- */
-
 var Messages = new Meteor.Collection("messages")
 
-var MessageBoard = React.createClass({
+var Chat = React.createClass({
   mixins:[ReactMeteor.Mixin],
   getMeteorState: function(){
-    var name = ''
-    if (Session.get('name')) name = Session.get('name')
     return {
-      messages: Messages.find({}).fetch().reverse(),
-      name: name
+      messages: Messages.find({}).fetch().reverse()
     }
   },
-  startMeteorSubscriptions: function() {
-    Meteor.subscribe('messages')
-  },
   addMessage: function(message){
-    var messages = this.state.messages
-    Session.set('name', message.name)
     Messages.insert(message)
   },
   render: function(){
     return (
       <div>
-        <NewMessage messages={this.state.messages} addMessageCb={this.addMessage} username={this.state.name}/>
+        <h1>Chat</h1>
+        <p>This application uses React and Meteor. This example is for the Core React book.</p>
+        <NewMessage
+          messages={this.state.messages}
+          addMessageCb={this.addMessage}/>
         <MessageList messages={this.state.messages} />
       </div>
     )
@@ -41,7 +32,6 @@ var NewMessage = React.createClass({
       name: fD(this.refs.name).value,
       message: fD(this.refs.message).value
     })
-    // fD(this.refs.name).value = ''
     fD(this.refs.message).value = ''
   },
   keyup: function (e) {
@@ -53,9 +43,23 @@ var NewMessage = React.createClass({
       <div className="row-fluid" id="new-message">
         <div className="span12">
           <form className="well form-inline" onKeyUp={this.keyup}>
-            <input type="text" name="username" className="input-small" placeholder="Azat" ref="name" defaultValue={this.props.username}/>
-            <input type="text" name="message" className="input-small" placeholder="Hello!" ref="message" />
-            <a id="send" className="btn btn-primary" onClick={this.addMessage}>POST</a>
+            <input
+              type="text"
+              name="username"
+              className="input-small"
+              placeholder="Azat"
+              ref="name"/>
+            <input
+              type="text"
+              name="message"
+              className="input-small"
+              placeholder="Hello!"
+              ref="message" />
+            <a id="send"
+              className="btn btn-primary"
+              onClick={this.addMessage}>
+              POST
+            </a>
           </form>
         </div>
       </div>
@@ -99,9 +103,8 @@ var MessageList = React.createClass({
 
 if (Meteor.isClient) {
   Meteor.startup(function() {
-    React.render(<MessageBoard />, document.getElementById('content'))
+    React.render(<Chat />, document.body)
   })
-
 }
 
 
