7f750ad65af9859b56410da03ffe056b65cf4907
Azat Mardan <hi@azat.co>
2015-10-05 13:43:18 -0700

    evolve code

--- ch1/hello-js-world-jquery/script.js
+++ ch1/hello-js-world-jquery/script.js
@@ -1,9 +1,9 @@
-var HelloWorld = function(name){ 
-  return jQuery('<h1>Hello '+name+' world!</h1>')
+var HelloWorld = function(name){
+  return jQuery('<h1>Hello ' + name + ' world!</h1>')
 }
 
-
-jQuery('#example')
+jQuery('#example').html('<div>')
+jQuery('#example div')
   .append(HelloWorld('Ember.js'))
   .append(HelloWorld('Backbone.js'))
   .append(HelloWorld('Angular.js'))
--- ch2/menu/script.jsx
+++ ch2/menu/script.jsx
@@ -1,11 +1,25 @@
+window.isLoggedIn =true
 var Menu = React.createClass({
   render: function(){
-    var menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us']
+    var menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us'],
+      loginLogoutLink
+    if (window.isLoggedIn) {
+      loginLogoutLink = <a href="/logout">Log out</a>
+    } else {
+      loginLogoutLink = <a href="/login">Log in</a>
+    }
     return (
       <div>
-      {menus.map(function(v,i){
-        return <Link label={v}/>
-      })}
+        {menus.map(function(v,i){
+          return <Link label={v}/>
+        })}
+        <div>Status: {window.isLoggedIn ? 'Logged in' : 'Logged out'}</div>
+        <div>{loginLogoutLink}</div>
+        <div>{function(){
+          if (window.isLoggedIn) return <a href='/logout'>Log out</a>
+          else return <a href='/login'>Log in</a>
+          }()}
+        </div>
       </div>
     )
 }})
