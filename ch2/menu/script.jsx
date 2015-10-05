1b1f379b710556dae9ff87553da8ecc9073423d6
Azat Mardan <hi@azat.co>
2015-10-05 14:27:09 -0700

    formatting

--- ch2/menu/script.jsx
+++ ch2/menu/script.jsx
@@ -1,25 +1,15 @@
-window.isLoggedIn =true
 var Menu = React.createClass({
   render: function(){
-    var menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us'],
-      loginLogoutLink
-    if (window.isLoggedIn) {
-      loginLogoutLink = <a href="/logout">Log out</a>
-    } else {
-      loginLogoutLink = <a href="/login">Log in</a>
-    }
+    var menus = ['Home',
+      'About',
+      'Services',
+      'Portfolio',
+      'Contact us']
     return (
       <div>
         {menus.map(function(v,i){
           return <Link label={v}/>
         })}
-        <div>Status: {window.isLoggedIn ? 'Logged in' : 'Logged out'}</div>
-        <div>{loginLogoutLink}</div>
-        <div>{function(){
-          if (window.isLoggedIn) return <a href='/logout'>Log out</a>
-          else return <a href='/login'>Log in</a>
-          }()}
-        </div>
       </div>
     )
 }})
