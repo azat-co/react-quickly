147c54e64b67c230bd7d3fbf1be8e77118277c73
Azat Mardan <hi@azat.co>
2015-10-05 14:27:19 -0700

    add if else

---
+++ ch2/menu-if-else/script.jsx
@@ -0,0 +1,46 @@
+window.isLoggedIn =true
+var Menu = React.createClass({
+  render: function(){
+    var menus = ['Home',
+        'About',
+        'Services',
+        'Portfolio',
+        'Contact us'
+      ],
+      loginLogoutLink
+    if (window.isLoggedIn) {
+      loginLogoutLink = <a href="/logout">Log out</a>
+    } else {
+      loginLogoutLink = <a href="/login">Log in</a>
+    }
+    return (
+      <div>
+        {menus.map(function(v,i){
+          return <Link label={v}/>
+        })}
+        <div>Status: {window.isLoggedIn ? 'Logged in' : 'Logged out'}</div>
+        <div>{loginLogoutLink}</div>
+        <div>{function(){
+          if (window.isLoggedIn) return <a href='/logout'>Log out</a>
+          else return <a href='/login'>Log in</a>
+          }()}
+        </div>
+      </div>
+    )
+}})
+
+var Link = React.createClass({
+  render: function () {
+    var url='/'
+      + this.props.label
+        .toLowerCase()
+        .trim()
+        .replace(' ', '-')
+    return <a href={url}>
+      {this.props.label}
+      <br/>
+    </a>
+  }
+})
+
+React.render(<Menu />, document.getElementById('menu'))
