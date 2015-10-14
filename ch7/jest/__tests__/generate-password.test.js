358d3ca0d9ab7450fdb595c63ecb4d272ad0caee
azat-co <hi@azat.co>
2015-10-13 17:17:15 -0700

    update jest example

--- ch7/jest/__tests__/generate-password.test.js
+++ ch7/jest/__tests__/generate-password.test.js
@@ -1,11 +1,8 @@
 jest.dontMock('../generate-password.js')
 
-beforeEach(function(){
+describe('method generatePassword', function(){
   var password
   generatePassword = require('../generate-password')
-})
-
-describe('method generatePassword', function(){
   it('returns a generated password of lower-case characters and numbers with the length of 8', function(){
     password = generatePassword()
     expect(password).toMatch(/^[a-z0-9]{8}$/)
