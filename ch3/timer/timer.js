e0bae6215e8241aa71de064faef51276ea28b342
Azat Mardan <hi@azat.co>
2015-11-13 14:14:41 -0800

    changes

--- ch3/timer/timer.js
+++ ch3/timer/timer.js
@@ -4,14 +4,19 @@ var Timer = React.createClass({
   displayName: 'Timer',
 
   render: function render() {
-    if (this.props.time == null) return React.createElement('div', null);
+    if (this.props.time == 0) {
+      document.getElementById('end-of-time').play();
+    }
+    if (this.props.time == null || this.props.time == 0) return React.createElement('div', null);
     return React.createElement(
       'h1',
       null,
+      'Time left: ',
       this.props.time
     );
   }
 });
+
 var TimerWrapper = React.createClass({
   displayName: 'TimerWrapper',
 
@@ -22,27 +27,32 @@ var TimerWrapper = React.createClass({
     clearInterval(this.state.int);
     var _this = this;
     var int = setInterval(function () {
-      console.log('2');
+      console.log('2: Inside of setInterval');
       var tl = _this.state.time - 1;
       if (tl == 0) clearInterval(int);
       _this.setState({ time: tl });
     }, 1000);
-    console.log('1');
+    console.log('1: After setInterval');
     return this.setState({ time: time, int: int });
   },
   render: function render() {
     return React.createElement(
       'div',
-      null,
+      { className: 'row-fluid' },
       React.createElement(
         'h2',
         null,
         'Timer'
       ),
-      React.createElement(Button, { time: '5', startTimer: this.startTimer }),
-      React.createElement(Button, { time: '10', startTimer: this.startTimer }),
-      React.createElement(Button, { time: '15', startTimer: this.startTimer }),
-      React.createElement(Timer, { time: this.state.time })
+      React.createElement(
+        'div',
+        { className: 'btn-group', role: 'group' },
+        React.createElement(Button, { time: '5', startTimer: this.startTimer }),
+        React.createElement(Button, { time: '10', startTimer: this.startTimer }),
+        React.createElement(Button, { time: '15', startTimer: this.startTimer })
+      ),
+      React.createElement(Timer, { time: this.state.time }),
+      React.createElement('audio', { id: 'end-of-time', src: 'flute_c_long_01.wav', preload: 'auto' })
     );
   }
 });
@@ -56,11 +66,11 @@ var Button = React.createClass({
   render: function render() {
     return React.createElement(
       'button',
-      { onClick: this.startTimer },
+      { type: 'button', className: 'btn-default btn', onClick: this.startTimer },
       this.props.time,
       ' seconds'
     );
   }
 });
 
-React.render(React.createElement(TimerWrapper, null), document.body);
+ReactDOM.render(React.createElement(TimerWrapper, null), document.getElementById('timer-app'));
--- ch3/timer/timer.jsx
+++ ch3/timer/timer.jsx
@@ -1,9 +1,13 @@
 var Timer = React.createClass({
    render: function() {
-     if (this.props.time == null) return <div/>
-     return <h1>{this.props.time}</h1>
+     if (this.props.time == 0) {
+       document.getElementById('end-of-time').play()
+     }
+     if (this.props.time == null || this.props.time == 0) return <div/>
+     return <h1>Time left: {this.props.time}</h1>
     }
 })
+
 var TimerWrapper = React.createClass({
   getInitialState: function () {
     return {time: null, int: null}
@@ -11,23 +15,26 @@ var TimerWrapper = React.createClass({
   startTimer: function (time) {
     clearInterval(this.state.int)
     var _this= this
-    var int = setInterval(function(){
-      console.log('2')
+    var int = setInterval(function() {
+      console.log('2: Inside of setInterval')
       var tl = _this.state.time - 1
       if (tl == 0) clearInterval(int)
       _this.setState({time: tl})
     }, 1000)
-    console.log('1')
+    console.log('1: After setInterval')
     return this.setState({time: time, int: int})
   },
   render: function() {
     return (
-      <div>
+      <div className="row-fluid">
         <h2>Timer</h2>
-        <Button time="5" startTimer={this.startTimer}/>
-        <Button time="10" startTimer={this.startTimer}/>
-        <Button time="15" startTimer={this.startTimer}/>
+        <div className="btn-group" role="group" >
+          <Button time="5" startTimer={this.startTimer}/>
+          <Button time="10" startTimer={this.startTimer}/>
+          <Button time="15" startTimer={this.startTimer}/>
+        </div>
         <Timer time={this.state.time}/>
+      <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
       </div>
     );
   }
@@ -38,11 +45,13 @@ var Button = React.createClass({
     return this.props.startTimer(this.props.time)
   },
   render: function () {
-    return <button onClick={this.startTimer}>{this.props.time} seconds</button>
+    return <button type="button" className='btn-default btn' onClick={this.startTimer}>
+      {this.props.time} seconds
+    </button>
   }
 })
 
-React.render(
+ReactDOM.render(
   <TimerWrapper/>,
-  document.body
+  document.getElementById('timer-app')
 )
