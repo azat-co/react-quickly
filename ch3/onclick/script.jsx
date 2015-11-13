6b04ae03c38b1d2ae96f64c79ee47ef353248aed
Azat Mardan <hi@azat.co>
2015-11-13 13:05:47 -0800

    changes

--- ch3/onclick/script.jsx
+++ ch3/onclick/script.jsx
@@ -1,20 +1,22 @@
 var Content = React.createClass({
-  getInitialState: function(){
+  getInitialState: function() {
     return {counter: 0}
   },
-  click: function(e){
+  click: function(e) {
     this.setState({counter: ++this.state.counter})
   },
   render: function() {
     return (
       <div>
-        <button onClick={this.click}>Don't click me {this.state.counter} times! </button>
+        <button onClick={this.click}>
+          Don't click me {this.state.counter} times!
+        </button>
       </div>
     )
   }
 })
 
-React.render(
+ReactDOM.render(
   <Content />,
   document.getElementById('content')
 );
