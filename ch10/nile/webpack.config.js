de5b66e10428c2dc0265a9ea1a1b439b387271e1
Azat Mardan <hi@azat.co>
2015-12-21 11:03:23 -0800

    add changes :sleeping:

--- ch10/nile/webpack.config.js
+++ ch10/nile/webpack.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   entry: "./app.jsx",
   output: {
-      path: __dirname,
-      filename: "bundle.js"
+    path: __dirname,
+    filename: "bundle.js"
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['es2015']
+        },
         loader: 'babel' // 'babel-loader' is also a legal name to reference
       }
     ]
