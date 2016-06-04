c41db05345e1afb3e014b267052a4a19d36348b8
Azat Mardan <hi@azat.co>
2016-06-03 19:05:43 -0700

    add changes :rage2:

--- ch03/hello-js-world-jsx/js/script.jsx
+++ ch03/hello-js-world-jsx/js/script.jsx
@@ -11,13 +11,13 @@ ReactDOM.render(
       frameworkName='Ember.js'
       title='A framework for creating ambitious web applications.'/>,
     <HelloWorld
-      id= 'backbone'
-      frameworkName= 'Backbone.js'
-      title= 'Backbone.js gives structure to web applications...'/>
+      id='backbone'
+      frameworkName= Backbone.js'
+      title='Backbone.js gives structure to web applications...'/>
     <HelloWorld
-      id= 'angular'
-      frameworkName= 'Angular.js'
-      title= 'Superheroic JavaScript MVW Framework'/>
+      id='angular'
+      frameworkName='Angular.js'
+      title='Superheroic JavaScript MVW Framework'/>
   </div>,
   document.getElementById('content')
 )
