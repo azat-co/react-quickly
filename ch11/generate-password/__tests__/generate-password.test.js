66c6398695f6c04bdc2987df7a1b82896898d4a2
Azat Mardan <hi@azat.co>
2016-07-18 12:25:11 -0700

    add changes :anguished:

--- ch11/generate-password/__tests__/generate-password.test.js
+++ ch11/generate-password/__tests__/generate-password.test.js
@@ -1,10 +1,11 @@
 jest.dontMock('../generate-password.js')
 
-describe('method generatePassword', function(){
-  var password
+describe('method generatePassword', ()=>{
+  let password
   generatePassword = require('../generate-password')
-  it('returns a generated password of lower-case characters and numbers with the length of 8', function(){
+  it('returns a generated password of lower-case characters and numbers with the length of 8', (done)=>{
     password = generatePassword()
     expect(password).toMatch(/^[a-z0-9]{8}$/)
+    done()
   })
 })
