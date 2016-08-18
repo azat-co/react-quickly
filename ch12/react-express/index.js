ea43840f0ec7bb3f13cbf97dfb044c7700b7eb8d
Azat Mardan <hi@azat.co>
2016-08-18 11:39:48 -0700

    add changes :tired_face:

---
+++ ch12/react-express/index.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const errorHandler = require('errorhandler')
+const http = require('http')
+const https = require('https')
+
+const React = require('react')
+require('babel-register')({
+  presets: [ 'react' ]
+})
+const ReactDOMServer = require('react-dom/server')
+const About = React.createFactory(require('./components/about.jsx'))
+
+app.set('view engine', 'hbs')
+app.get('/', (request, response, next)=>{
+  response.send('Hello!')
+})
+app.get('/about', (request, response, next) => {
+  const aboutHTMl = ReactDOMServer.renderToString(About())
+  response.render('about', {about: aboutHTMl})
+})
+
+// app.get('/about', (request, response, next) => {
+//   response.send(`<div>
+//   <a href="http://Node.University" target="_blank">Node.University</a>
+//    is home to top-notch Node education which brings joy to JavaScript engineers.
+// </div>`)
+//})
+
+app.all('*', (request, response, next)=> {
+  response.status(404).send('Not found... did you mean to go to /about instead?')
+})
+app.use((error, request, response, next) => {
+  console.error(request.url, error)
+  response.send('Wonderful, something went wrong...')
+})
+
+app.use(errorHandler)
+
+http.createServer(app)
+  .listen(8080)
+
+
+try {
+  const options = {
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.crt')
+  }
+} catch (e) {
+  console.warn('Create server.key and server.crt for HTTPS')
+}
+if (typeof options != 'undefined')
+  https.createServer(app, options)
+    .listen(443)
