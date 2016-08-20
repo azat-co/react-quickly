bbeb2434bc4189b899b8d7efd39e334d72507bf2
Azat Mardan <hi@azat.co>
2016-08-20 14:52:51 -0700

    add changes :non-potable_water:

---
+++ ch09/email-webpack/jsx/app.jsx
@@ -0,0 +1,9 @@
+const React = require('react')
+const ReactDOM = require ('react-dom')
+const Content = require('./content.jsx')
+
+
+ReactDOM.render(
+  <Content />,
+  document.getElementById('content')
+)
---
+++ ch09/email-webpack/jsx/content.jsx
@@ -0,0 +1,34 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props)
+    this.submit = this.submit.bind(this)
+    this.prompt = 'Please enter your email to win $1,000,000.'
+  }
+  submit(event) {
+    let emailAddress = this.refs.emailAddress
+    let comments = this.refs.comments
+    console.log(ReactDOM.findDOMNode(emailAddress).value)
+    console.log(ReactDOM.findDOMNode(comments).value)
+  }
+  render() {
+    return (
+      <div className="well">
+        <p>{this.prompt}</p>
+        <div className="form-group">
+          Email: <input ref="emailAddress" className="form-control" type="text" placeholder="hi@azat.co"/>
+        </div>
+        <div className="form-group">
+          Comments: <textarea ref="comments" className="form-control"  placeholder="I like your website!"/>
+        </div>
+        <div className="form-group">
+          <a className="btn btn-primary" value="Submit" onClick={this.submit}>Submit</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = Content
