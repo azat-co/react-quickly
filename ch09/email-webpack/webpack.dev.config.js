a26de6268ca985a489655bce5c2ca890c07baa26
Azat Mardan <hi@azat.co>
2016-08-24 09:24:00 +0000

    add changes :whale2:

--- ch09/email-webpack/webpack.dev.config.js
+++ ch09/email-webpack/webpack.dev.config.js
@@ -14,6 +14,7 @@ module.exports = {
   devtool: '#sourcemap',
   module: {
     loaders: [
+      { test: /\.css$/, loader: 'style!css' },
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
