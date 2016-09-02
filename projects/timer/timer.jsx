021991f6edb8bbe865f63814ca8980641992edd7
Azat Mardan <hi@azat.co>
2016-09-01 18:55:45 -0700

    add changes :violin:

--- projects/timer/timer.jsx
+++ projects/timer/timer.jsx
@@ -1,9 +1,10 @@
 class Timer extends React.Component {
   render() {
-  if (this.props.timeLeft == 0) {
-    document.getElementById('end-of-time').play()
-  }
-  if (this.props.timeLeft == null || this.props.timeLeft == 0) return <div/>
+    if (this.props.timeLeft == 0) {
+      document.getElementById('end-of-time').play()
+    }
+    if (this.props.timeLeft == null || this.props.timeLeft == 0)
+      return <div/>
     return <h1>Time left: {this.props.timeLeft}</h1>
   }
 }
