8680cfbf7007aae76e4ed65c8084772d65083447
Azat Mardan <hi@azat.co>
2016-10-26 13:03:08 -0400

    add changes :hammer:

--- ch11/timer/timer.jsx
+++ ch11/timer/timer.jsx
@@ -2,17 +2,27 @@ class Timer extends React.Component {
   render() {
     if (this.props.timeLeft == 0) {
       document.getElementById('end-of-time').play()
-    }
-    if (this.props.timeLeft == null || this.props.timeLeft == 0)
       return <div/>
+    }
     return <h1>Time left: {this.props.timeLeft}</h1>
+  }
+}
+
+class Button extends React.Component {
+  startTimer(event) {
+    return this.props.startTimer(this.props.time)
+  }
+  render() {
+    return <button type="button" className='btn-default btn' onClick={this.startTimer.bind(this)}>
+      {this.props.time} seconds
+    </button>
   }
 }
 
 class TimerWrapper extends React.Component {
   constructor(props) {
     super(props)
-    this.state =  {timeLeft: null, timer: null}
+    this.state =  {timeLeft: 0, timer: null}
     this.startTimer = this.startTimer.bind(this)
   }
   startTimer(timeLeft) {
@@ -39,17 +49,6 @@ class TimerWrapper extends React.Component {
       <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
       </div>
     )
-  }
-}
-
-class Button extends React.Component {
-  startTimer(event) {
-    return this.props.startTimer(this.props.time)
-  }
-  render() {
-    return <button type="button" className='btn-default btn' onClick={this.startTimer.bind(this)}>
-      {this.props.time} seconds
-    </button>
   }
 }
 
