de982edcb22a3d9bb5afe0cec912765b0d658a30
Azat Mardan <hi@azat.co>
2016-10-29 13:22:47 -0700

    add changes :minibus:

--- ch14/redux-netflix/webpack.config.js
+++ ch14/redux-netflix/webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: {
