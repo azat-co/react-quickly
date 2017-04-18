4757276f5a604759eaff843cb12c495c9f2e05a1
Azat Mardan <hi@azat.co>
2017-04-17 23:03:13 -0700

    update to react 15.5 chapter 6

--- ch06/add-sub/jsx/add-sub.jsx
+++ ch06/add-sub/jsx/add-sub.jsx
@@ -1,10 +1,10 @@
 
-var AddSub = React.createClass({
-  getInitialState(){
-    return {
-      value: this.props.defaultValue
-    }
-  },
+class AddSub extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state.value = this.props.defaultValue
+    this.handleResize = this.handleResize.bind(this)
+  }
   getStyle(i, m) {
     let value = i*m
     return {
@@ -13,23 +13,21 @@ var AddSub = React.createClass({
       left: value,
       right: value,
     }
-  },
-  componentDidMount: function() {
+  }
+  componentDidMount() {
     window.addEventListener('resize', this.handleResize)
-  },
-
-  componentWillUnmount: function() {
+  }
+  componentWillUnmount() {
     window.removeEventListener('resize', this.handleResize)
-  },
-  handleResize: function(e) {
+  }
+  handleResize(e) {
     let w = 1+ Math.round(window.innerWidth / 300)
     this.setState({
       taggerStyle: {top: this.props.order*w*10, width: w*10, height: w*10},
       textStyle: {left: w*13, fontSize: 7*w}
     })
-  },
-
-  render: function(){
+  }
+  render(){
     return <div>
       <div className="radio-tagger" style={this.state.taggerStyle}>
         <input type="radio" name={this.props.name} id={this.props.id}>
@@ -46,4 +44,4 @@ var AddSub = React.createClass({
       </div>
     </div>
   }
-})
+}
