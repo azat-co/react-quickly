ac7961f0e2951948a92744a054f7c9fe4b34ebbf
Azat Mardan <hi@azat.co>
2017-04-17 21:46:37 -0700

    update ch12/email-webpack to react 15, webpack 2

--- ch12/email-webpack/webpack.dev.config.js
+++ ch12/email-webpack/webpack.dev.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack')
 module.exports = {
   entry: [
     'webpack-dev-server/client/?http://localhost:8080',
-    'webpack/hot/dev-server',
     './jsx/app.jsx'
   ],
   output: {
@@ -14,11 +13,11 @@ module.exports = {
   devtool: '#sourcemap',
   module: {
     loaders: [
-      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loaders: ['react-hot', 'babel']
+        loaders: ['react-hot-loader', 'babel-loader']
       }
     ]
   },
