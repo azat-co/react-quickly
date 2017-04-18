94878093a0c4d6225f43e6c4361aaf04ffad4a59
Azat Mardan <hi@azat.co>
2017-04-17 20:42:47 -0700

    update all

--- ch16/testutils/__tests__/render-props.js
+++ ch16/testutils/__tests__/render-props.js
@@ -1,17 +1,17 @@
 jest.dontMock('react')
-jest.dontMock('react-addons-test-utils')
+jest.dontMock('react-dom')
 
 describe('HelloWorld', ()=>{
-  const TestUtils = require('react-addons-test-utils')
+  const TestUtils = require('react-dom/test-utils')
   const React = require('react')
 
   it('has props', (done)=>{
 
-    const HelloWorld = React.createClass({
+    class HelloWorld extends React.Component {
       render() {
         return <div>{this.props.children}</div>
       }
-    })
+    }
     let hello = TestUtils.renderIntoDocument(<HelloWorld>Hello Node!</HelloWorld>)
     expect(hello.props).toBeDefined()
     console.log('my hello props:', hello.props) // my div: Hello Node!
--- ch16/testutils/__tests__/scry-div.js
+++ ch16/testutils/__tests__/scry-div.js
@@ -1,17 +1,17 @@
 jest.dontMock('react')
-jest.dontMock('react-addons-test-utils')
+jest.dontMock('react-dom')
 
 describe('HelloWorld', ()=>{
-  const TestUtils = require('react-addons-test-utils')
+  const TestUtils = require('react-dom/test-utils')
   const React = require('react')
 
   it('has a div', (done)=>{
 
-    const HelloWorld = React.createClass({
+    class HelloWorld extends React.Component {
       render() {
         return <div>{this.props.children}</div>
       }
-    })
+    }
     let hello = TestUtils.renderIntoDocument(<HelloWorld>Hello Node!</HelloWorld>)
     expect( TestUtils.scryRenderedDOMComponentsWithTag(hello, 'div').length).toBe(1)
     console.log('found this many divs: ', TestUtils.scryRenderedDOMComponentsWithTag(hello, 'div').length)
