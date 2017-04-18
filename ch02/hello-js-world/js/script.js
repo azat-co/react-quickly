4b995a012f30af4bfc6128d1ea310ae01f57980c
Azat Mardan <hi@azat.co>
2017-04-17 19:43:46 -0700

    add changes :chicken:

--- ch02/hello-js-world/js/script.js
+++ ch02/hello-js-world/js/script.js
@@ -3,7 +3,7 @@ class HelloWorld extends React.Component {
     return React.createElement(
       'h1',
       this.props,
-      'Hello ' + this.props.frameworkName + ' world!!!'
+      'Hello ' + this.props.frameworkName + ' world!'
     )
   }
 }
