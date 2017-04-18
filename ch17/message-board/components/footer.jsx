6db9c7ff2c317bde8f1d0a19b43155f181cfd2dc
Azat Mardan <hi@azat.co>
2017-04-17 20:13:53 -0700

    update message board in ch17 to webpack 2, react 15, new mongodb

--- ch17/message-board/components/footer.jsx
+++ ch17/message-board/components/footer.jsx
@@ -1,16 +1,14 @@
 const React = require('react')
 
-module.exports = Footer = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <hr />
-        <div className="row-fluid">
-          <div className="span12">
-            <div>React Quickly by Azat (<a href="http://twitter.com/azat_co" target="_blank">@azat_co</a>)</div>
-          </div>
+module.exports = Footer = (props)=>{
+  return (
+    <div>
+      <hr />
+      <div className="row-fluid">
+        <div className="span12">
+          <div>React Quickly by Azat (<a href="http://twitter.com/azat_co" target="_blank">@azat_co</a>)</div>
         </div>
       </div>
-    )
-  }
-})
+    </div>
+  )
+}
