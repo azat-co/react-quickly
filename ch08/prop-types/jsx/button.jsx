53bfe6e957d8d8d406bdfcef523ebcc401259fb0
Azat Mardan <hi@azat.co>
2017-04-20 12:17:00 -0700

    add changes :mans_shoe:

--- ch08/prop-types/jsx/button.jsx
+++ ch08/prop-types/jsx/button.jsx
@@ -7,7 +7,7 @@ class Button extends React.Component {
 Button.defaultProps = {buttonLabel: 'Submit'}
 
 Button.propTypes = {
-  handler:  PropTypes.func.isRequired,
+  handler: PropTypes.func.isRequired,
   title: PropTypes.string,
   email(props, propName, componentName) {
     let emailRegularExpression = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
