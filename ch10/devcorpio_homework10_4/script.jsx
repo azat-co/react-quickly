6f5a59ea2aaef5b216087890a90890d9beca860e
Alberto Delgado <adelgado.roda@gmail.com>
2017-12-03 14:41:48 +0100

    ch10 Homework 10.4, pass new properties to allow activate toggle with click and mouse interaction

--- ch10/devcorpio_homework10_4/script.jsx
+++ ch10/devcorpio_homework10_4/script.jsx
@@ -2,8 +2,26 @@ class Tooltip extends React.Component {
   constructor(props) {
     super(props)
     this.state = {opacity: false}
-    this.toggle = this.toggle.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleMouseInteraction = this.handleMouseInteraction.bind(this)
   }
+
+  handleClick() {
+    if (!this.props.allowToggleWithClick) {
+      return false;
+    }
+
+    this.toggle();
+  }
+
+  handleMouseInteraction() {
+    if (!this.props.allowToggleWithMouseInteraction) {
+      return false;
+    }
+
+    this.toggle();
+  }
+
   toggle() {
     const tooltipNode = ReactDOM.findDOMNode(this)
     this.setState({
@@ -12,6 +30,7 @@ class Tooltip extends React.Component {
       left: tooltipNode.offsetLeft
     })
   }
+
   render() {
     const style = {
       zIndex: (this.state.opacity) ? 1000 : -1000,
@@ -21,7 +40,7 @@ class Tooltip extends React.Component {
     }
     return (
       <div style={{display: 'inline'}}>
-        <span style={{color: 'blue'}} onClick={this.toggle} onMouseEnter={this.toggle} onMouseOut={this.toggle}>
+        <span style={{color: 'blue'}} onClick={this.handleClick} onMouseEnter={this.handleMouseInteraction} onMouseOut={this.handleMouseInteraction}>
           {this.props.children}
         </span>
         <div className="tooltip bottom" style={style} role="tooltip">
@@ -35,8 +54,18 @@ class Tooltip extends React.Component {
   }
 }
 
+Tooltip.propTypes = {
+  allowToggleWithClick: PropTypes.bool,
+  allowToggleWithMouseInteraction: PropTypes.bool,
+};
+
+Tooltip.defaultProps = {
+  allowToggleWithClick: false,
+  allowToggleWithMouseInteraction: true,
+};
+
 ReactDOM.render(<div>
-  <Tooltip text="Master Express.js-The Node.js Framework For Your Web Development">Pro Express.js</Tooltip> was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress
+  <Tooltip text="Master Express.js-The Node.js Framework For Your Web Development" allowToggleWithClick={false} allowToggleWithMouseInteraction={true}>Pro Express.js</Tooltip> was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress
     after <Tooltip text="Practical Node.js: Building Real-World Scalable Web Apps">Practical Node.js</Tooltip>.
     ...
     The main focus of this post is to compare the four Node.js/Io.js frameworks: <Tooltip text="HTTP API server">Hapi</Tooltip>, <Tooltip text="Release the Kraken!">Kraken</Tooltip>, <Tooltip text="Sail away">Sails.js</Tooltip> and <Tooltip text="IBM of frameworks">Loopback</Tooltip>. There are many other frameworks to consider, but I had to draw the line somewhere.
