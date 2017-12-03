20f8dd49920d2ba26fa47ebbfda1aa951af41db6
Alberto Delgado <adelgado.roda@gmail.com>
2017-12-03 21:21:57 +0100

    ch11 Homework 11.7, component Timer converted to stateless

---
+++ ch11/devcorpio_homework11_7/timer.jsx
@@ -0,0 +1,60 @@
+const Timer = (props) => {
+  if (props.timeLeft == 0) {
+    document.getElementById('end-of-time').play()
+  }
+  if (props.timeLeft == null || props.timeLeft == 0)
+    return <div/>
+  return <h1>Time left: {props.timeLeft}</h1>
+};
+
+class Button extends React.Component {
+  startTimer(event) {
+    return this.props.startTimer(this.props.time)
+  }
+  render() {
+    return <button
+      type="button"
+      className='btn-default btn'
+      onClick={()=>{this.props.startTimer(this.props.time)}}>
+      {this.props.time} seconds
+    </button>
+  }
+}
+
+class TimerWrapper extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state =  {timeLeft: null, timer: null}
+    this.startTimer = this.startTimer.bind(this)
+  }
+  startTimer(timeLeft) {
+    clearInterval(this.state.timer)
+    let timer = setInterval(() => {
+      console.log('2: Inside of setInterval')
+      var timeLeft = this.state.timeLeft - 1
+      if (timeLeft == 0) clearInterval(timer)
+      this.setState({timeLeft: timeLeft})
+    }, 1000)
+    console.log('1: After setInterval')
+    return this.setState({timeLeft: timeLeft, timer: timer})
+  }
+  render() {
+    return (
+      <div className="row-fluid">
+        <h2>Timer</h2>
+        <div className="btn-group" role="group" >
+          <Button time="5" startTimer={this.startTimer}/>
+          <Button time="10" startTimer={this.startTimer}/>
+          <Button time="15" startTimer={this.startTimer}/>
+        </div>
+        <Timer timeLeft={this.state.timeLeft}/>
+      <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <TimerWrapper/>,
+  document.getElementById('timer-app')
+)
