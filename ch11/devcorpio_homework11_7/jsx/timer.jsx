3fbf57c50d256c3cd957c5a7a16620aa99695f9c
Alberto Delgado <adelgado.roda@gmail.com>
2017-12-04 21:11:54 +0100

    ch11 Homework 11.7, implement a slider button that changes with every time interval

--- ch11/devcorpio_homework11_7/jsx/timer.jsx
+++ ch11/devcorpio_homework11_7/jsx/timer.jsx
@@ -13,10 +13,22 @@ class TimerWrapper extends React.Component {
     let timer = setInterval(() => {
       console.log('2: Inside of setInterval')
       var timeLeft = this.state.timeLeft - 1
-      if (timeLeft == 0) clearInterval(timer)
+      if (timeLeft == 0) {
+        clearInterval(timer)
+        dispatchEvent(new CustomEvent('timerFinished', {}));
+      }
       this.setState({timeLeft: timeLeft})
+      dispatchEvent(new CustomEvent('timeIncremented', {}))
     }, 1000)
     let selectedTime = originalTime ? originalTime : this.state.selectedTime
+
+    dispatchEvent(new CustomEvent('timerStarted', {
+      detail: {
+        timeLeft: timeLeft,
+        selectedTime: selectedTime,
+      }
+    }))
+
     return this.setState({timeLeft: timeLeft, timer: timer, selectedTime: selectedTime})
   }
   stopTimer() {
@@ -36,6 +48,8 @@ class TimerWrapper extends React.Component {
       timeLeft: null,
       timer: null,
     })
+
+    dispatchEvent(new CustomEvent('timerFinished', {}));
   }
   resetTimer() {
     this.startTimer(this.state.selectedTime)
@@ -46,21 +60,23 @@ class TimerWrapper extends React.Component {
       <div className="row-fluid">
         <h2>Timer</h2>
         <div className="btn-group" role="group" >
-          <Button time="300" startTimer={this.startTimer}/>
+          <Button time="20" startTimer={this.startTimer}/>
           <Button time="600" startTimer={this.startTimer}/>
           <Button time="900" startTimer={this.startTimer}/>
         </div>
         <Timer timeLeft={this.state.timeLeft}/>
         {this.state.timeLeft > 0 &&
-        <div className="btn-group" role="group">
-          {this.state.timer === null
-            ?
-            <button className="btn-success btn" onClick={this.resumeTimer}>Resume</button>
-            :
-            <button className="btn-warning btn" onClick={this.stopTimer}>Pause</button>
-          }
-          <button className="btn-danger btn" onClick={this.cancelTimer}>Cancel</button>
-          <button className="btn-primary btn" onClick={this.resetTimer}>Reset</button>
+        <div>
+          <div className="btn-group" role="group">
+            {this.state.timer === null
+              ?
+              <button className="btn-success btn" onClick={this.resumeTimer}>Resume</button>
+              :
+              <button className="btn-warning btn" onClick={this.stopTimer}>Pause</button>
+            }
+            <button className="btn-danger btn" onClick={this.cancelTimer}>Cancel</button>
+            <button className="btn-primary btn" onClick={this.resetTimer}>Reset</button>
+          </div>
         </div>
         }
       <TimerSound />
